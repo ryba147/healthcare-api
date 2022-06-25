5dd81a0f3c9ac2c94b1b6b30f03291b87d20fe8f
ryba147 <kot3myp3uk@gmail.com>
2022-06-25 18:14:32 +0300

    Fixed installed apps. Makefile commands

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'patient',
-    'doctor',
+    'patients',
+    'doctors',
 ]
 
 MIDDLEWARE = [
